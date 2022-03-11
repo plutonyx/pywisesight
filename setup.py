6b68c5c024203311bdc61208b071ce147a7e94d7
Thaweesak Chusri <thaweesak@nimeyo.com>
2022-03-11 10:22:45 +0800

    :bookmark: Bump version: 0.0.4 → 0.0.5 [CI SKIP]

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ CURRENT_DIR = pathlib.Path(__file__).parent
 
 setup(
     name='wisesight',
-    version='0.0.4',
+    version='0.0.5',
     url='https://github.com/plutonyx/wisesight',
     author='P4L3K1N6',
     author_email='p4l3k1n6@gmail.com',
